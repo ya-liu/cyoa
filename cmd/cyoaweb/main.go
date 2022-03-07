60aeb4651b6e936d4dfb1afa8fc9361563fe518e
Ya Liu <sarahl0216@gmail.com>
2022-03-07 00:44:16 -0800

    refactored json parsing functionality

--- cmd/cyoaweb/main.go
+++ cmd/cyoaweb/main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"encoding/json"
 	"flag"
 	"fmt"
 	"os"
@@ -19,11 +18,8 @@ func main() {
 		panic(err)
 	}
 
-	// Decoder takes an io.Reader
-	d := json.NewDecoder(f)
-	var story cyoa.Story
-	// Decode takes a pointer, can't just use story here
-	if err := d.Decode(&story); err != nil {
+	story, err := cyoa.JsonStory(f)
+	if err != nil {
 		panic(err)
 	}
 
--- story.go
+++ story.go
@@ -1,5 +1,21 @@
 package cyoa
 
+import (
+	"encoding/json"
+	"io"
+)
+
+func JsonStory(r io.Reader) (Story, error) {
+	// Decoder takes an io.Reader
+	d := json.NewDecoder(r)
+	var story Story
+	// Decode takes a pointer, can't just use story here
+	if err := d.Decode(&story); err != nil {
+		return nil, err
+	}
+	return story, nil
+}
+
 type Story map[string]Chapter
 
 type Chapter struct {
