53f80b6232befe020951910dc1c6d611bc621948
Ya Liu <sarahl0216@gmail.com>
2022-04-11 22:16:33 -0700

    add html template

--- story.go
+++ story.go
@@ -5,6 +5,26 @@ import (
 	"io"
 )
 
+var defaultHandlerTmpl = `
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>Choose Your Own Adventure</title>
+	</head>
+	<body>
+		<h1>{{.Title}}</h1>
+		{{range .Paragraphs}}
+			<p>{{.}}</p>
+		{{end}}
+		<ul>
+			{{range .Options}}
+				<li><a href="/{{.Chapter}}">{{.Text}}</a></li>
+			{{end}}
+		</ul>
+	</body>
+</html>`
+
 func JsonStory(r io.Reader) (Story, error) {
 	// Decoder takes an io.Reader
 	d := json.NewDecoder(r)
