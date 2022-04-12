65762403f9ea76207c58d863e6ea4bb6f0e21f65
Ya Liu <sarahl0216@gmail.com>
2022-04-11 22:36:23 -0700

    implemented rendering of the story

--- cmd/cyoaweb/main.go
+++ cmd/cyoaweb/main.go
@@ -3,12 +3,15 @@ package main
 import (
 	"flag"
 	"fmt"
+	"log"
+	"net/http"
 	"os"
 
 	"github.com/ya-liu/cyoa"
 )
 
 func main() {
+	port := flag.Int("port", 3000, "the port to run cyoa app")
 	filename := flag.String("file", "gopher.json", "The JSON file with the CYOA story")
 	flag.Parse()
 	fmt.Printf("Using the story in %s.\n", *filename)
@@ -23,5 +26,7 @@ func main() {
 		panic(err)
 	}
 
-	fmt.Printf("%+v\n", story)
+	h := cyoa.NewHandler(story)
+	fmt.Printf("Starting the server on port: %d\n", *port)
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", *port), h))
 }
