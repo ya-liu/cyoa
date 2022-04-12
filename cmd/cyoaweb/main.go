21af2a4d57c9239f9296f88d3c9f569a569efac4
Ya Liu <sarahl0216@gmail.com>
2022-04-11 23:55:23 -0700

    custom paths

--- cmd/cyoaweb/main.go
+++ cmd/cyoaweb/main.go
@@ -3,9 +3,11 @@ package main
 import (
 	"flag"
 	"fmt"
+	"html/template"
 	"log"
 	"net/http"
 	"os"
+	"strings"
 
 	"github.com/ya-liu/cyoa"
 )
@@ -26,7 +28,43 @@ func main() {
 		panic(err)
 	}
 
-	h := cyoa.NewHandler(story, cyoa.WithTemplate(nil))
+	tpl := template.Must(template.New("").Parse(storyTmpl))
+
+	h := cyoa.NewHandler(story,
+		cyoa.WithTemplate(tpl),
+		cyoa.WithPathFunc(pathFn),
+	)
+	// serve mux adds a `/story/` by default to paths that don't start with /story/
+	mux := http.NewServeMux()
+	mux.Handle("/story/", h)
 	fmt.Printf("Starting the server on port: %d\n", *port)
-	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", *port), h))
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", *port), mux))
 }
+
+func pathFn(r *http.Request) string {
+	path := strings.TrimSpace(r.URL.Path)
+	if path == "/story/" || path == "/story" {
+		path = "/story/intro"
+	}
+	return path[len("/story"):]
+}
+
+var storyTmpl = `
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>Choose Your Own Adventure</title>
+	</head>
+	<body>
+		<h1>{{.Title}}</h1>
+		{{range .Paragraphs}}
+			<p>{{.}}</p>
+		{{end}}
+		<ul>
+			{{range .Options}}
+				<li><a href="/story/{{.Chapter}}">{{.Text}}</a></li>
+			{{end}}
+		</ul>
+	</body>
+</html>`
